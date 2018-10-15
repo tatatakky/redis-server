abed3416d794a8204b589715b220b9e8d8b63881
tatatakky <s1240232@gmail.com>
2018-10-15 14:52:53 +0900

    [modify] err exception

--- test/judge_redis.py
+++ test/judge_redis.py
@@ -2,10 +2,9 @@ def exists_key(r, k):
     #key値がある場合はgetter関数が呼ばれる
     from get_redis import getter
     from set_redis import setter
-    truth = r.exists(k)
-    if truth is True:
+    import sys
+    try:
         val = getter(r, k)
         return val
-    else:
-        pass
-        #setter(r, k)
+    except Exception:
+        return "Error : Not having the KEY ({}) in redis-db".format(k)
