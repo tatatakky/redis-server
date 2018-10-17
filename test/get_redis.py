b3c670b209dfd030b5bb416bcb3391d4021e288b
tatatakky <s1240232@gmail.com>
2018-10-17 15:36:16 +0900

    [modify] confirm key exists

--- test/get_redis.py
+++ test/get_redis.py
@@ -1,7 +1,7 @@
 def getter(r, key):
     #return r.get(key).decode()
-    li = [v.decode() for v in r.lrange(key, 0, -1)]
-    if len(li) > 0:
-        return li
-    else:
-        return "Not Having the KEY: {}".format(key)
+    return [v.decode() for v in r.lrange(key, 0, -1)]
+    #if len(li) > 0:
+    #    return li
+    #else:
+    #    return "Not Having the KEY: {}".format(key)
--- test/judge_redis.py
+++ test/judge_redis.py
@@ -2,6 +2,7 @@ def exists_key(r, k):
     #key値がある場合はgetter関数が呼ばれる
     from get_redis import getter
     from set_redis import setter
-    val = getter(r, k)
-    return val
-
+    if r.exists(k) is True:
+        return getter(r, k)
+    else:
+        return "No Exist the key:{}".format(k)
