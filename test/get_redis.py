fbbde62664955d70b467a3f499071fb9e5686014
tatatakky <s1240232@gmail.com>
2018-10-18 23:26:41 +0900

    [modify]

--- test/get_redis.py
+++ test/get_redis.py
@@ -1,7 +1,2 @@
 def getter(r, key):
-    #return r.get(key).decode()
     return [v.decode() for v in r.lrange(key, 0, -1)]
-    #if len(li) > 0:
-    #    return li
-    #else:
-    #    return "Not Having the KEY: {}".format(key)
