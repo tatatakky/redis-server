2633a483cb7b50b0fa9261f0cf4a7915e90c6d27
tatatakky <s1240232@gmail.com>
2018-11-17 14:06:00 +0900

    [modify] dddd

--- main/judge_redis.py
+++ main/judge_redis.py
@@ -1,7 +1,7 @@
 def exists_key(r, k):
     #key値がある場合はgetter関数が呼ばれる
     from get_redis import getter
-    from set_redis import setter
+    # from set_redis import setter
     from flask import Flask, jsonify, abort, make_response
     if r.exists(k) is True:
         return getter(r, k)
